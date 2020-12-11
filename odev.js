bd4f66bc1b041ce8feec271ea5dc13b13dbf20a8
Mahmut <mahmutt.yildiz@outlook.com>
2020-12-11 21:45:10 +0000

    homework finished.

--- odev.js
+++ odev.js
@@ -3,15 +3,15 @@
   Asagidaki fonksiyonu "Hello, John ciktisi verecek sekilde duzenleyiniz."
  */
 
-var person = {
-  name: "Joe",
-  message: function () {    
+const person = {
+  name: "John",
+  message: function () {
     console.log("Hello, " + this.name)
   }
 }
 
-var messageFunc = person.message
-messageFunc();
+const messageFn = person.message;
+messageFn.call(person)
 
 
 /*  
@@ -22,17 +22,17 @@ messageFunc();
   60
   sonuclarini yazdiracak sekilde duzenleyiniz.
 */
-var numbers = {
-  numbers: [[10,20,30], 2],
-  multiply: function(){
-    this.numbers[0].map(function(number, numberIndex){
-        const result = number * this.numbers[1];
-        console.log(result)
+const numbers = {
+  numbersArray: [[10, 20, 30], 2],
+  multiply: function () {
+    this.numbersArray[0].map((number, numberIndex) => {
+      const result = number * this.numbersArray[1];
+      console.log(result)
     })
   }
 };
 
-numbers.multiply();
+console.log(numbers.multiply());
 
 
 /* 
@@ -42,24 +42,66 @@ numbers.multiply();
   Ornek : isValidName("John") true donmeli
   Ornek : isValidName(" J ohn") false donmeli
 */
-function isValidName(name){
+const isValidName = (name) => {
+  const checkLetters = /^[A-Za-z]+$/;
+  return checkLetters.test(name)
+};
 
+const isValidName = (name) => {
+  if (typeof (name) === "string") {
+    const trimmedName = name.trim();
+    const splittedName = trimmedName.split(" ");
+    for (let i = 0; i < splittedName.length; i++) {
+      if (splittedName[i].length <= 1) {
+        break;
+      } else {
+        return true;
+      }
+
+    }
+  }
 }
+
+console.log(isValidName("Frank") === true);
+console.log(isValidName(false) === false);
+console.log(isValidName(null) === false);
+console.log(isValidName(undefined) === false);
+console.log(isValidName("") === false);
+console.log(isValidName("  \t\n") === false);
+console.log(isValidName("X") === false);
 
 /*
   Odev 4:
   Asagidaki katilimSaati fonksionu 2 arguman almaktadir.
   dersSayisi: ogrencinin katildigi ders sayisi
   dersSaati: her bir dersin dakika cinsinden suresi
-  katilimSaati fonkisyonu kac derse gerilidigi ve her bir dersin kac dakika surdugu bilgisini alip sonuc olarak toplamda kac dakika derse girildigini donmelidir.
+  katilimSaati fonksiyonu kac derse girildigi ve her bir dersin kac dakika surdugu bilgisini alip sonuc olarak toplamda kac dakika derse girildigini donmelidir.
   - iki arguman de number veya string olarak verilebilir.
   - Sayi olmayan ya da sayiya cevrilemeyen argumanlar var ise hata donulmelidir.
   Ornek: katilimSaati(3, 30) 90 sonucunu vermelidir.
   Ornek: katilimSaati("3", 20) 60 sonucunu vermelidir.
   Ornek: katilimSaati("5", "30") 150 sonucunu vermelidir.
 */
-function katilimSaati(dersSayisi, dersSuresi){
+const katilimSaati = (dersSayisi, dersSuresi) => {
+  if (dersSayisi.length == 0 || dersSuresi.length == 0) {
+    return false;
+  }
 
+  if (typeof +dersSayisi == "boolean" || typeof +dersSuresi == "boolean") {
+    return false;
+  }
+
+  if (typeof +dersSayisi !== "number" || typeof +dersSuresi !== "number") {
+    return false;
+  }
+
+  if (!isFinite(+dersSuresi) || !isFinite(+dersSayisi)) {
+    return false;
+  }
+
+  return Math.floor(dersSayisi) * Math.floor(dersSuresi);
 }
+
+
 
 
